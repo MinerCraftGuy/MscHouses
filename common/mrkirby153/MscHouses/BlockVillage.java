d6c49cee81e4cd47db8dacba612d8eaff907fc94
mrkirby153 <mr.austinwhyte@gmail.com>
2013-03-03 10:33:52 -0800

    Removed excess imports

--- common/mrkirby153/MscHouses/BlockVillage.java
+++ common/mrkirby153/MscHouses/BlockVillage.java
@@ -1,20 +1,12 @@
 package mrkirby153.MscHouses;
 
-import java.util.List;
 import java.util.Random;
 
-import cpw.mods.fml.common.IWorldGenerator;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.IProgressUpdate;
-import net.minecraft.world.ChunkPosition;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.gen.structure.MapGenVillage;
 
 public class BlockVillage extends Block{
 	
