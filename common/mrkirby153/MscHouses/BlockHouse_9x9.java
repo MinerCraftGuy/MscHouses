c8b2f8997ec69da36ac1fb4eda4b7ffd88414c6c
mrkirby153 <mr.austinwhyte@gmail.com>
2013-03-03 16:40:07 -0800

    Fixed chat message displaying twice and added item damage

--- common/mrkirby153/MscHouses/BlockHouse_9x9.java
+++ common/mrkirby153/MscHouses/BlockHouse_9x9.java
@@ -3,6 +3,7 @@ package mrkirby153.MscHouses;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -21,7 +22,15 @@ public class BlockHouse_9x9 extends Block{
 	@Override
 	public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9) {
 		MscHouses.h.ninebynine(par1World, par2, par3, par4);
-		par5EntityPlayer.sendChatToPlayer("Please install your door and bed. Found in the chest.");
+		System.out.println("Blah Blah Blah");
+		if(par5EntityPlayer != null  && par5EntityPlayer.getItemInUse() != null && par5EntityPlayer.getItemInUse().itemID == MscHouses.HouseTool.itemID){
+			ItemStack item = par5EntityPlayer.getItemInUse();
+		//	item.damageItem(1, par5EntityPlayer);
+			item.setItemDamage(item.getItemDamage() +1);
+		}
+		if(!par1World.isRemote){
+			par5EntityPlayer.sendChatToPlayer("Please install your door and bed. Found in the chest.");
+		}
 		return true;
 	}
 
