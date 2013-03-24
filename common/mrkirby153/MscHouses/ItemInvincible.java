ddf8cb9bc8f8bbcb9af6847c9cac92b3e683ee10
mrkirby153 <mr.austinwhyte@gmail.com>
2013-03-24 14:26:58 -0700

    Rewrote item for 2 minute recharge.

--- common/mrkirby153/MscHouses/ItemInvincible.java
+++ common/mrkirby153/MscHouses/ItemInvincible.java
@@ -16,12 +16,14 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemInvincible extends Item{
 	private Icon icon;
-	private boolean enabled;
-	private int timeLeft;
+	private int damage = 2400;
+	private boolean enabled = false;
 	public ItemInvincible(int par1) {
 		super(par1);
 		this.setCreativeTab(MscHouses.tabHouse);
 		this.setMaxStackSize(1);
+		this.setMaxDamage(damage);
+		this.setFull3D();
 	}
 
 	@Override
@@ -36,56 +38,26 @@ public class ItemInvincible extends Item{
 		// TODO Auto-generated method stub
 		return icon;
 	}
-	@Override
-	public void onUpdate(ItemStack item, World world, Entity entity, int par4, boolean par5) {
-		boolean enabled = NBTHelper.getBoolean(item, "enabled");
-		int timeLeft = NBTHelper.getInteger(item, "timeLeft");
-		
-		if(entity instanceof EntityPlayer){
-			EntityPlayer player = (EntityPlayer) entity;
-			if(enabled && timeLeft != 0){
-			//	player.addPotionEffect(new PotionEffect(2, 20, 0, true));
-			//	player.addPotionEffect(new PotionEffect(13, 20, 0, true));
-				player.extinguish();
-				if(player.getHealth() < 10){
-					player.heal(1);
-				}
-				timeLeft--;
-			}
-			if(timeLeft == 0){
-				checkForFuel(player);
-			}
-		}
-		NBTHelper.setBoolean(item, "enabled", enabled);
-		NBTHelper.setInteger(item, "timeLeft", timeLeft);
+@Override
+public void onUpdate(ItemStack par1ItemStack, World par2World,
+		Entity par3Entity, int par4, boolean par5) {
+	// TODO Auto-generated method stub
+	if(par1ItemStack.getItemDamage() > 0){
+		par1ItemStack.setItemDamage(par1ItemStack.getItemDamage() -1);
 	}
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void addInformation(ItemStack item,
-			EntityPlayer player, List list, boolean par4) {
-		boolean enabled = NBTHelper.getBoolean(item, "enabled");
-		list.add("Keeps you 'INVINCIBLE'! Runs on ender Pearls");
-		list.add("Time left on current charge: " + Math.floor(timeLeft/20) );
-		list.add("Activated? " +enabled);
-	}
+}
 
-	private void checkForFuel(EntityPlayer player){
-		for(int slot = 0; slot < player.inventory.getSizeInventory(); slot++){
-			ItemStack fuel = player.inventory.getStackInSlot(slot);
-			if(fuel != null){
-				if(fuel.itemID == Item.enderPearl.itemID){
-					fuel.stackSize--;
-					timeLeft = 1200;
-					break;
-				}
-			}
-		}
-	}
+
 	@Override
-	public ItemStack onItemRightClick(ItemStack item, World world,
-			EntityPlayer par3EntityPlayer) {
-		boolean enabled = !NBTHelper.getBoolean(item, "enabled");
-		par3EntityPlayer.sendChatToPlayer("Value of Enabled: " + enabled);
+	public ItemStack onItemRightClick(ItemStack item, World world, EntityPlayer player) {
+		if(!player.capabilities.isCreativeMode || item.getItemDamage() == 0){
+		item.damageItem(2400, player);
+	//	player.addPotionEffect(new PotionEffect(2, 1200, 0, true));
+		player.addPotionEffect(new PotionEffect(13, 2400, 0, true));
+		player.addPotionEffect(new PotionEffect(11, 2400, 5, true));
+		player.addPotionEffect(new PotionEffect(12, 2400, 5, true));
+		player.addPotionEffect(new PotionEffect(9, 120, 5, true));
+		}
 		return item;
 	}
 }
