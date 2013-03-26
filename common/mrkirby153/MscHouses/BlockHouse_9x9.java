045a0f3cf9e9fc1b81333f04f913dbcd17a2e68a
mrkirby153 <mr.austinwhyte@gmail.com>
2013-03-25 17:23:02 -0700

    Changed textures and added GNU license header

--- common/mrkirby153/MscHouses/BlockHouse_9x9.java
+++ common/mrkirby153/MscHouses/BlockHouse_9x9.java
@@ -2,20 +2,28 @@ package mrkirby153.MscHouses;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import cpw.mods.fml.common.registry.GameRegistry;
-
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+/**
+ * MscHouses
+ * @author mrkirby153
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
 public class BlockHouse_9x9 extends Block{
 
 	protected BlockHouse_9x9(int par1)
-    {
-        super(par1, Material.ground);
-        this.setCreativeTab(MscHouses.tabHouse);
-        GameRegistry.registerBlock(this, "9x9");
-    }
+	{
+		super(par1, Material.ground);
+		this.setCreativeTab(MscHouses.tabHouse);
+		GameRegistry.registerBlock(this, "9x9");
+	}
 	@Override
-	public String getTextureFile() {
-		// TODO Auto-generated method stub
-		return "mods/MscHouses/textures/blocks/block9x9.png";
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister par1IconRegister) {
+		this.blockIcon = par1IconRegister.registerIcon("MscHouses:Block9x9");
 	}
 }
