65cce5b716ac12518989bc4eb6f49ed277a79486
mrkirby153 <mr.austinwhyte@gmail.com>
2013-03-25 17:26:01 -0700

    Changed Texture call

--- common/mrkirby153/MscHouses/ItemInvincible.java
+++ common/mrkirby153/MscHouses/ItemInvincible.java
@@ -13,10 +13,15 @@ import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-
+/**
+ * MscHouses
+ * @author mrkirby153
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
 public class ItemInvincible extends Item{
 	private Icon icon;
-	private int damage = 2400;
+	private int damage = 6000;
 	private boolean enabled = false;
 	public ItemInvincible(int par1) {
 		super(par1);
@@ -28,36 +33,33 @@ public class ItemInvincible extends Item{
 
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void func_94581_a(IconRegister iconRegister){
-		icon = iconRegister.func_94245_a("MscHouses:invincible");
+	public void updateIcons(IconRegister iconRegister) {
+		// TODO Auto-generated method stub
+		iconIndex = iconRegister.registerIcon("MscHouses:ItemInvincible");
 	}
 
 	@Override
-	@SideOnly(Side.CLIENT)
-	public Icon getIconFromDamage(int par1) {
+	public void onUpdate(ItemStack par1ItemStack, World par2World,
+			Entity par3Entity, int par4, boolean par5) {
 		// TODO Auto-generated method stub
-		return icon;
+		if(par1ItemStack.getItemDamage() > 0){
+			par1ItemStack.setItemDamage(par1ItemStack.getItemDamage() -1);
+		}
 	}
-@Override
-public void onUpdate(ItemStack par1ItemStack, World par2World,
-		Entity par3Entity, int par4, boolean par5) {
-	// TODO Auto-generated method stub
-	if(par1ItemStack.getItemDamage() > 0){
-		par1ItemStack.setItemDamage(par1ItemStack.getItemDamage() -1);
-	}
-}
+	
 
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack item, World world, EntityPlayer player) {
-		if(!player.capabilities.isCreativeMode || item.getItemDamage() == 0){
-		item.damageItem(2400, player);
-	//	player.addPotionEffect(new PotionEffect(2, 1200, 0, true));
-		player.addPotionEffect(new PotionEffect(13, 2400, 0, true));
-		player.addPotionEffect(new PotionEffect(11, 2400, 5, true));
-		player.addPotionEffect(new PotionEffect(12, 2400, 5, true));
-		player.addPotionEffect(new PotionEffect(9, 120, 5, true));
+		if(!player.capabilities.isCreativeMode && item.getItemDamage() == 0){
+			item.damageItem(6000, player);
+			//	player.addPotionEffect(new PotionEffect(2, 1200, 0, true));
+			player.addPotionEffect(new PotionEffect(13, 2400, 0, true));
+			player.addPotionEffect(new PotionEffect(11, 2400, 5, true));
+			player.addPotionEffect(new PotionEffect(12, 2400, 5, true));
+			player.addPotionEffect(new PotionEffect(9, 120, 5, true));
 		}
 		return item;
 	}
+	
 }
