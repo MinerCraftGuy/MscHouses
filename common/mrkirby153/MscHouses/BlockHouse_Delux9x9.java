667c0ff01b7d3979e876a7295ff4939db6d99e7b
mrkirby153 <mr.austinwhyte@gmail.com>
2013-03-29 14:53:02 -0700

    EVEN MORE code cleanup

--- common/mrkirby153/MscHouses/BlockHouse_Delux9x9.java
+++ common/mrkirby153/MscHouses/BlockHouse_Delux9x9.java
@@ -14,15 +14,15 @@ import cpw.mods.fml.relauncher.SideOnly;
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
 public class BlockHouse_Delux9x9 extends Block {
-    protected BlockHouse_Delux9x9(int par1) {
-        super(par1, Material.ground);
-        this.setCreativeTab(MscHouses.tabHouse);
-        GameRegistry.registerBlock(this, "delux9x9");
-    }
+	protected BlockHouse_Delux9x9(int par1) {
+		super(par1, Material.ground);
+		this.setCreativeTab(MscHouses.tabHouse);
+		GameRegistry.registerBlock(this, "delux9x9");
+	}
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister) {
-        blockIcon = par1IconRegister.registerIcon("MscHouses:Block9x9_Delux");
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister par1IconRegister) {
+		blockIcon = par1IconRegister.registerIcon("MscHouses:Block9x9_Delux");
+	}
 }
