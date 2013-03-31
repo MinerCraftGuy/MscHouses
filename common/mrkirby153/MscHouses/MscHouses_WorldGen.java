2a32b4a106c442e9b857d25737d913c0802a4f20
mrkirby153 <mr.austinwhyte@gmail.com>
2013-03-31 11:55:01 -0700

    Major changes to generaton code

--- common/mrkirby153/MscHouses/MscHouses_WorldGen.java
+++ common/mrkirby153/MscHouses/MscHouses_WorldGen.java
@@ -11,33 +11,22 @@ public class MscHouses_WorldGen implements IWorldGenerator{
 
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-//		System.out.println("Generating Terrain...");
 		switch(world.provider.dimensionId){
 			case -1: generateNeter(world, random,  chunkX*16, chunkZ*16);
 			case 0: generateSurface(world, random, chunkX*16, chunkZ*16);
 		}
-		generateHouse(world, random, chunkX*16,chunkZ*16);
 		
 	}
 
-	private void generateHouse(World world, Random random, int x, int z) {
-	//	int chances = random.nextInt(300);
-	//	int chances = random.nextInt(40);
-	//	if(chances <= 30){
-			MscHouses.h.ninbynineDelux(world, x+random.nextInt(16), 50, z+random.nextInt());
-	//	}
-		
-	}
+	private void generateSurface(World world, Random random, int i, int j) {
 
-	private void generateSurface(World world, Random random, int x, int z) {
-		for(int i = 1; i < 30; i++){
-			int xCoord, yCoord, zCoord;
-			xCoord = x+random.nextInt(16);
-			yCoord = 36-random.nextInt(3);
-			zCoord = z+random.nextInt(16);
-			(new WorldGenMinable(MscHouses.CopperOre.blockID, 3000)).generate(world, random, xCoord, yCoord, zCoord);
+		for(int k = 1; k < 15; k++){
+		int xCoord1 = i+random.nextInt(16);
+		int zCoord1 = j+random.nextInt(16);
+		int yCoord1 = 30;
+		(new WorldGenMinable(MscHouses.CopperOre.blockID, 16)).generate(world, random, xCoord1, yCoord1, zCoord1);
+	//	System.out.println("Copper has been generated @" +xCoord1 +" " + yCoord1 + " "+ zCoord1);
 		}
-		
 	}
 
 	private void generateNeter(World world, Random random, int i, int j) {
