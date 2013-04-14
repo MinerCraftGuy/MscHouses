680a28d3139fd99ee35127d4a769834d5c0c8d9f
mrkirby153 <mr.austinwhyte@gmail.com>
2013-04-14 15:43:31 -0700

    Added Build number to mod version and added required Forge version

--- common/mrkirby153/MscHouses/lib/Reference.java
+++ common/mrkirby153/MscHouses/lib/Reference.java
@@ -4,8 +4,8 @@ public class Reference {
 
 	public static final String MOD_ID = "MscHouses";
 	public static final String MOD_NAME = "MscHouses";
-	public static final String VERSION_NUMBER = "@VERSION@";
-	public static final String DEPENDANCIES = "";
+	public static final String VERSION_NUMBER = "@VERSION@(@BUILD_NUMBER@)";
+	public static final String DEPENDANCIES = "required-after:Forge@[7.7.1.650,)";
 	public static final String CHANNEL_NAME = MOD_ID;
 	public static final int VERSION_CHECK_ATTEMPTS = 3;
 }
