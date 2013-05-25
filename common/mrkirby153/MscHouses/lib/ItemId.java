24ff44f31c14581b85f9616e9a93c83d332f97f3
mrkirby153 <mr.austinwhyte@gmail.com>
2013-05-25 10:31:04 -0700

    Added stuff for moifyer moduels

--- common/mrkirby153/MscHouses/lib/ItemId.java
+++ common/mrkirby153/MscHouses/lib/ItemId.java
@@ -9,6 +9,7 @@ public class ItemId {
 	public static int ITEM_INVINCIBLE_DEFAULT = 4003;
 	public static int ITEM_PCB_DEFAULT = 4004;
 	public static int ITEM_MODUEL_DEFAULT = 4005;
+	public static int ITEM_MODIFYER_DEFAULT = 4006;
 	
 	//Current IDs
 	public static int DEBUG;
@@ -17,5 +18,6 @@ public class ItemId {
 	public static int ITEM_INVINCBLE;
 	public static int ITEM_PCB;
 	public static int ITEM_MODUEL;
+	public static int ITEM_MODIFYER;
 
 }
