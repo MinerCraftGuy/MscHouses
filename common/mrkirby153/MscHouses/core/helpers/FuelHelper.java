fd1bf3a3b7c2756e1defef51f86f90c3569d5def
mrkirby153 <mr.austinwhyte@gmail.com>
2013-06-15 10:03:02 -0700

    Removed console spam

--- common/mrkirby153/MscHouses/core/helpers/FuelHelper.java
+++ common/mrkirby153/MscHouses/core/helpers/FuelHelper.java
@@ -1,27 +1,24 @@
 package mrkirby153.MscHouses.core.helpers;
 
-import java.util.ArrayList;
-import java.util.logging.Level;
-
-import mrkirby153.MscHouses.core.handlers.FuelHandler;
+import mrkirby153.MscHouses.api.FuelRegistry;
 import net.minecraft.item.Item;
-
+/**
+ * 
+ * Msc Houses
+ *
+ * FuelHelper
+ *
+ * @author mrkirby153
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
 public class FuelHelper {
 
 	public static void registerFuels(){
 		//Adds fuels
 		//Default fuels
-		FuelHandler.addFuel(Item.coal);
-		FuelHandler.addFuel(Item.bucketLava);
-		FuelHandler.addFuel(Item.netherStar);
-		//Gets and prints the arraylist as an ArrayList
-		ArrayList<Item> fuels_arrayList = FuelHandler.getValidFuelsAsArrayList();
-		LogHelper.log(Level.SEVERE, fuels_arrayList.toString());
-		//gets and prints the arraylist as a Array;
-		Item[] fuels_List = FuelHandler.getValidFulesAsList();
-		for(int i = 1; i < fuels_List.length; i++){
-			LogHelper.log(Level.SEVERE, fuels_List[i].toString());
-		}
+		FuelRegistry.addFuel(Item.coal);
+		FuelRegistry.addFuel(Item.bucketLava);
+		FuelRegistry.addFuel(Item.netherStar);
 	}
 	
 }
