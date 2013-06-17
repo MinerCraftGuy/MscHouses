78cc5501508ec509c4f3fe94e4a6f94a636bb78d
mrkirby153 <mr.austinwhyte@gmail.com>
2013-06-17 09:59:05 -0700

    Updated requiredAfter to newer forge.

--- common/mrkirby153/MscHouses/lib/Reference.java
+++ common/mrkirby153/MscHouses/lib/Reference.java
@@ -13,7 +13,7 @@ public class Reference {
 	public static final String MOD_ID = "MscHouses";
 	public static final String MOD_NAME = "MscHouses";
 	public static final String VERSION_NUMBER = "@VERSION@(@BUILD_NUMBER@)";
-	public static final String DEPENDANCIES = "required-after:Forge@[7.8.0.712,)";
+	public static final String DEPENDANCIES = "required-after:Forge@[7.8.0.730,)";
 	public static final String CHANNEL_NAME = MOD_ID;
 	public static final int VERSION_CHECK_ATTEMPTS = 3;
 	public static final String CLIENT_PROXY_CLASS = "mrkirby153.MscHouses.core.network.ClientProxy";
