8d13753388c57cc07e5f5a98e13e43724821b48e
mrkirby153 <mr.austinwhyte@gmail.com>
2013-08-23 18:38:07 -0700

    Deleted Comments

--- common/mrkirby153/MscHouses/core/helpers/LocalMaterialHelper.java
+++ common/mrkirby153/MscHouses/core/helpers/LocalMaterialHelper.java
@@ -8,17 +8,9 @@ import net.minecraft.block.Block;
 
 
 public class LocalMaterialHelper {
-	/*private static Block[] materials = {Block.blockClay, Block.blockDiamond, Block.blockEmerald, Block.blockGold,
-			Block.blockIron, Block.blockLapis, Block.blockRedstone, Block.brick, Block.cloth, Block.cobblestone,
-			Block.dirt, Block.glass, Block.glowStone, Block.netherBrick, Block.obsidian, Block.planks, Block.redstoneLampIdle,
-			Block.sandStone, Block.stone, Block.snow, Block.stoneBrick, Block.thinGlass, Block.tnt, Block.whiteStone, Block.wood};*/
 	private static Block[] blocks = Block.blocksList;
 	private static ArrayList<Integer> bl= MaterialRegistry.getBlacklisted();
 	public static void init(){
-		// LogHelper.log(Level.INFO, "Loading " + materials.length + " material recipies!");
-		//for(int i = 1; i < materials.length; i++){
-		//	MaterialRegistry.registerBlock(materials[i]);
-		//}
 		LogHelper.log(Level.INFO, "Loading "+blocks.length+" material recipies!");
 		for(int i=1; i < blocks.length; i++){
 			if(blocks[i] != null){
