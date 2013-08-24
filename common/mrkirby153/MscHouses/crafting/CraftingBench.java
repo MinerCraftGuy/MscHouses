fab8a1c956283827dc77a441511818d14f078b76
mrkirby153 <mr.austinwhyte@gmail.com>
2013-08-23 18:37:56 -0700

    Added module  recipes after they got removed

--- common/mrkirby153/MscHouses/crafting/CraftingBench.java
+++ common/mrkirby153/MscHouses/crafting/CraftingBench.java
@@ -24,13 +24,19 @@ public class CraftingBench {
 	public static void init(){
 		GameRegistry.addRecipe(new ItemStack(MscHouses.PCB, 1), new Object[]{
 			"X#X", "XXX", "X#X", 'X', Item.ingotIron, '#', Item.redstone});
-			Block[] customBlock = MaterialRegistry.getValidBlocksAsArray();
-			for(int i = 0; i < customBlock.length; i++){
-				GameRegistry.addRecipe(new ItemStack(MscHouses.modifyer, 1, i), new Object[]{"XXX","XYX","XZX", 'X', customBlock[i], 'Y', Item.enderPearl, 'Z', MscHouses.PCB});
-			}
-		GameRegistry.addRecipe(new ShapedOreRecipe(MscHouses.PCB, true, new Object[]{ "X#X","XXX","X#X",'X', "ingotCopper", '#', Item.redstone}));
-			LogHelper.log(Level.INFO, "Successfully Initalized Crafting Recipies!");
+		GameRegistry.addRecipe(new ItemStack(MscHouses.moduel, 1, 0), new Object[]{"###", "@X$", "###", '#', Item.redstone, '@', MscHouses.PCB, '$', Item.ingotIron});
+		GameRegistry.addRecipe(new ItemStack(MscHouses.moduel, 1, 1), new Object[]{"XXX", "XOX", "XXX", 'X', Block.wood, 'O', MscHouses.PCB});
+		GameRegistry.addRecipe(new ItemStack(MscHouses.moduel, 1, 2), new Object[]{"###", " X ", "###", 'X', MscHouses.moduel, '#', Block.stone});
+		GameRegistry.addRecipe(new ItemStack(MscHouses.moduel, 1, 3), new Object[]{"###", " X ", "###", 'X', MscHouses.moduel, '#', Item.ingotIron});
+		GameRegistry.addRecipe(new ItemStack(MscHouses.moduel, 1, 4), new Object[]{"BFB", " M ", "BOB", 'M', MscHouses.moduel, 'B', Block.brick, 'O', Block.obsidian});
+		GameRegistry.addRecipe(new ItemStack(MscHouses.moduel, 1, 5), new Object[]{"BDB", "BXB", "BOB", 'X', MscHouses.moduel, 'B', Block.bookShelf, 'O', Block.obsidian, 'D', Item.diamond});
+		Block[] customBlock = MaterialRegistry.getValidBlocksAsArray();
+		for(int i = 0; i < customBlock.length; i++){
+			GameRegistry.addRecipe(new ItemStack(MscHouses.modifyer, 1, i), new Object[]{"XXX","XYX","XZX", 'X', customBlock[i], 'Y', Item.enderPearl, 'Z', MscHouses.PCB});
 		}
-
-
+		GameRegistry.addRecipe(new ShapedOreRecipe(MscHouses.PCB, true, new Object[]{ "X#X","XXX","X#X",'X', "ingotCopper", '#', Item.redstone}));
+		LogHelper.log(Level.INFO, "Successfully Initalized Crafting Recipies!");
 	}
+
+
+}
