4db48825c62e97cf50b4d9c752ab36d6c9bb77dc
mrkirby153 <mr.austinwhyte@gmail.com>
2013-09-22 17:05:57 -0700

    Fixed Button
    Inventory is still messed up [WIP]

--- common/mrkirby153/MscHouses/block/GUI/GuiHouseGenerator.java
+++ common/mrkirby153/MscHouses/block/GUI/GuiHouseGenerator.java
@@ -5,8 +5,8 @@ import mrkirby153.MscHouses.block.tileEntity.TileEntityHouseGen;
 import mrkirby153.MscHouses.lib.ResourceFile;
 import mrkirby153.MscHouses.lib.Strings;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.StatCollector;
 
@@ -25,7 +25,7 @@ import cpw.mods.fml.relauncher.SideOnly;
  */
 @SideOnly(Side.CLIENT)
 public class GuiHouseGenerator extends GuiContainer{
-	
+
 	private GuiButton generate;
 	public GuiHouseGenerator (InventoryPlayer inventoryPlayer,
 			TileEntityHouseGen tileEntity) {
@@ -34,7 +34,10 @@ public class GuiHouseGenerator extends GuiContainer{
 		this.ySize = 176;
 		this.xSize = 176;
 	}
-
+	@Override
+	public void initGui() {
+	//	this.buttonList.add(generate = new GuiButton(0, this.width /2 + 20, this.height /2 - 65, 60, 20, "Generate"));
+	}
 	@Override
 	protected void drawGuiContainerForegroundLayer(int param1, int param2) {
 		//draw text and stuff here
@@ -42,7 +45,14 @@ public class GuiHouseGenerator extends GuiContainer{
 		fontRenderer.drawString(StatCollector.translateToLocal(Strings.RESOURCE_PREFIX+Strings.TILE_HOUSE_GEN), 8, 6, 4210752);
 		//draws "Inventory" or your regional equivalent
 		fontRenderer.drawString(StatCollector.translateToLocal("container.inventory"), 8, ySize - 96 + 2, 4210752);
-		this.buttonList.add(generate = new GuiButton(0, this.width /2 + 20, this.height /2 - 65, 60, 20, "Generate"));
+
+	}
+
+	@Override
+	protected void actionPerformed(GuiButton button) {
+		if(button.id==0){
+			this.mc.displayGuiScreen((GuiScreen)null);
+		}
 	}
 
 	@Override
@@ -51,7 +61,7 @@ public class GuiHouseGenerator extends GuiContainer{
 		//draw your Gui here, only thing you need to change is the path
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		this.mc.getTextureManager().bindTexture(ResourceFile.houseGen_Img);
-		
+
 		int x = (width - xSize) / 2;
 		int y = (height - ySize) / 2;
 		this.drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
